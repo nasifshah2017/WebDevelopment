82f6fc6d149ad076516692e647b2b9724d76f618
nasifshah2017 <33353457+nasifshah2017@users.noreply.github.com>
2018-04-29 23:50:16 -0300

    Add files via upload

---
+++ AgeCalculatorScript.js
@@ -0,0 +1,5 @@
+var Age = prompt("What is your age?");
+
+var age = Age*365
+
+alert("You are " + age + " days");
---
+++ script.js
@@ -0,0 +1,6 @@
+var userName = prompt("What is your name?");
+alert("Nice to meet you, " + userName);
+console.log("Also great to meet you, " + userName);
+
+//To have the bar to put down your name and stuff
+// you have to use the prompt command 
