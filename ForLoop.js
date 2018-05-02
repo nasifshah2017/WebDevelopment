4da067d241327382e381241d6aaaf4784b977b60
Nasif Shah <33353457+nasifshah2017@users.noreply.github.com>
2018-05-02 17:16:11 -0300

    Add files via upload

---
+++ ForLoop.js
@@ -0,0 +1,23 @@
+// Create numberA
+for(var numberA=-10; numberA<=19; numberA++){
+console.log(numberA);
+
+}
+
+//Create numberB
+for(var numberB = 10; numberB<=40; numberB+=2){
+	console.log(numberB);
+}
+
+//Create numberC
+for(var numberC = 301; numberC<=333; numberC+=2)
+{
+	console.log(numberC);
+}
+
+//Create numberD 
+for(var numberD=5; numberD<=50; numberD++)
+{
+	if(numberD%3===0 && numberD%5 ===0)
+	console.log(numberD);
+}
