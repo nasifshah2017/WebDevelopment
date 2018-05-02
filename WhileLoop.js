d101cfb4d19cb037a5531a04d3e7337c686110bc
nasifshah2017 <33353457+nasifshah2017@users.noreply.github.com>
2018-05-02 11:39:32 -0300

    Add files via upload

---
+++ WhileLoop.js
@@ -0,0 +1,40 @@
+// Create numberA
+var numberA = -10;
+
+while(numberA<=19)
+{
+	console.log(numberA);
+	numberA++;
+}
+
+//Create numberB
+var numberB = 10;
+
+while(numberB<=40)
+{
+	console.log(numberB);
+	numberB=numberB+2;
+}
+
+//Create numberC
+var numberC = 301;
+
+while(numberC<=333)
+{
+	console.log(numberC);
+	numberC=numberC+2;
+}
+
+//Create numberD and numberE
+var numberD = 5;
+//var numberE = 6;
+
+while(numberD<=50)
+{
+	if(numberD%3===0 || numberD%5 ===0)
+	console.log(numberD);
+	//console.log(numberE);
+	numberD++;
+
+	//numberE++;
+}
