f2d30bbca2582d8b5558ec4a17f2ac08a2dde148
Nasif Shah <33353457+nasifshah2017@users.noreply.github.com>
2018-05-05 23:57:40 -0300

    Add files via upload

--- list.js
+++ list.js
@@ -9,19 +9,49 @@ window.setTimeout(function(){
 	{
 		//handle input
 		if(input === "list") {
-			console.log(todos);
+			listTodos();
 	}
 
 	else if(input === "new") 
 	{
-		//ask for new todo
-		var newTodo = prompt("Enter new todo");
-		//add to todos array
-		todos.push(newTodo);
+		addTodo();
+	}
+
+	else if(input === "delete")
+	{
+		deleteTodo();
 	}
 		//ask again new input
 		var input = prompt("What would you like to do?");
 	}
 	console.log("OK, YOU QUIT THE APP");
+
+	function listTodos()
+	{
+		console.log("*****************")
+			todos.forEach(function(todo, i){ //"i" stands for index
+				console.log(i + ": " + todo);
+		});
+		console.log("*****************")
+	}
+
+	function addTodo()
+	{
+	//ask for new todo
+	var newTodo = prompt("Enter new todo");
+	//add to todos array
+	todos.push(newTodo);
+	console.log("Added Todo");
+	}
+
+	function deleteTodo()
+	{
+		//ask for index of todo to be deleted
+		var index = prompt("Enter index of todo to delete");
+		//delete that todo
+		//splice(): splice command starts deleting from the index number provided as argument and deletes the amount of contents provided as second argument
+		todos.splice(index,1); 
+		console.log("Deleted Todo");
+	}
 
 }, 500);
