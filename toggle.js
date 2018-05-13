743c6f59c34e9aeebd819b2cb59e6ff38e051d2f
Nasif Shah <33353457+nasifshah2017@users.noreply.github.com>
2018-05-13 13:03:59 -0300

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,15 @@
+var button = document.querySelector("button");
+var isPurple = false; //Initial boolean variable
+
+button.addEventListener("click", function(){
+	if(isPurple){
+		document.body.style.background = "white";
+		isPurple = false;
+	}
+	else
+	{
+		document.body.style.background = "purple";
+		isPurple = true;
+	}
+	
+});
