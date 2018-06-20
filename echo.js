a6ed8079bd8c43f8103d3e77b3cb33d77e5ab7e8
Nasif Shah <33353457+nasifshah2017@users.noreply.github.com>
2018-06-20 12:53:41 -0300

    Add files via upload

---
+++ echo.js
@@ -0,0 +1,8 @@
+function echo(str, num){
+    for(var i = 1; i <= num; i++){
+        console.log(str);
+    }
+}
+
+echo("Echo!!!", 10) //Prints "Echo!!!"" 10 times
+echo("Tater Tots", 3) //Prints "Tater Tots" 3 times 
