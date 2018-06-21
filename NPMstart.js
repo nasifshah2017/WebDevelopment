1466ba8455841291a82c66a3e33444e9176a8b94
Nasif Shah <33353457+nasifshah2017@users.noreply.github.com>
2018-06-21 12:21:27 -0300

    Add files via upload

---
+++ NPMstart.js
@@ -0,0 +1,162 @@
+var cat = require("cat-me");
+var joke = require("knock-knock-jokes");
+
+console.log(cat());
+
+console.log(joke());
+
+/**nasifshah:~/workspace $ ls
+Ex227/        client/        server.js
+IntroToNode/  node_modules/
+README.md     package.json
+nasifshah:~/workspace $ mkdir IntroToNode
+mkdir: cannot create directory ‘IntroToNode’: File exists
+nasifshah:~/workspace $ cd IntroToNode
+nasifshah:~/workspace/IntroToNode $ mkDir DemoApp
+bash: mkDir: command not found
+nasifshah:~/workspace/IntroToNode $ mkdir DemoApp
+nasifshah:~/workspace/IntroToNode $ cd DemoApp
+nasifshah:~/workspace/IntroToNode/DemoApp $ touch app.js
+nasifshah:~/workspace/IntroToNode/DemoApp $ ls
+app.js
+nasifshah:~/workspace/IntroToNode/DemoApp $ node app.js
+nasifshah:~/workspace/IntroToNode/DemoApp $ node app.js
+nasifshah:~/workspace/IntroToNode/DemoApp $ node app.js
+FROM APP.JS
+nasifshah:~/workspace/IntroToNode/DemoApp $ npm install cat-me
+chat-example@0.0.0 /home/ubuntu/workspace
+└─┬ cat-me@1.0.3
+  └─┬ yargs@3.32.0
+    ├── camelcase@2.1.1
+    ├─┬ cliui@3.2.0
+    │ ├─┬ strip-ansi@3.0.1
+    │ │ └── ansi-regex@2.1.1
+    │ └── wrap-ansi@2.1.0
+    ├── decamelize@1.2.0
+    ├─┬ os-locale@1.4.0
+    │ └─┬ lcid@1.0.0
+    │   └── invert-kv@1.0.0
+    ├─┬ string-width@1.0.2
+    │ ├── code-point-at@1.1.0
+    │ └─┬ is-fullwidth-code-point@1.0.0
+    │   └── number-is-nan@1.0.1
+    ├── window-size@0.1.4
+    └── y18n@3.2.1
+
+npm WARN chat-example@0.0.0 No repository field.
+npm WARN chat-example@0.0.0 No license field.
+nasifshah:~/workspace/IntroToNode/DemoApnasifshah:~/workspace/IntroToNode/DemoApm $ ls
+app.js
+nasifshah:~/workspace/IntroToNode/DemoApp $ clear
+nasifshah:~/workspace/IntroToNode/DemoApp $ node app.js
+  /\ ___ /\
+ (  o   o  )
+  \  >#<  /
+  /       \
+ /         \       ^
+|           |     //
+ \         /    //
+  ///  ///   --
+nasifshah:~/workspace/IntroToNode/DemoApp $ node app.js
+                           |        |
+                           |\      /|
+                           | \____/ |
+                           |  /\/\  |
+                          .'___  ___`.
+                         /  \|/  \|/  \
+        _.--------------( ____ __ _____)
+     .-' \  -. | | | | | \ ----\/---- /
+   .'\  | | / \` | | | |  `.  -'`-  .'
+  /`  ` ` '/ / \ | | | | \  `------'\
+ /-  `-------.' `-----.       -----. `---.
+(  / | | | |  )/ | | | )/ | | | | | ) | | )
+ `._________.'_____,,,/\_______,,,,/_,,,,/
+nasifshah:~/workspace/IntroToNode/DemoApp $ ls
+app.js  node_modules/
+nasifshah:~/workspace/IntroToNode/DemoApp $ npm install knock-knock-jokes
+/home/ubuntu/workspace/IntroToNode/DemoApp
+└── knock-knock-jokes@1.7.0
+
+npm WARN enoent ENOENT: no such file or directory, open '/home/ubuntu/workspace/IntroToNode/DemoApp/package.json'
+npm WARN DemoApp No description
+npm WARN DemoApp No repository field.
+npm WARN DemoApp No README data
+npm WARN DemoApp No license field.
+nasifshah:~/workspace/IntroToNode/DemoApp $ ls
+app.js  node_modules/
+nasifshah:~/workspace/IntroToNode/DemoApp $ ls node_modules
+active-x-obfuscator/
+ansi-regex/
+async/
+base64id/
+buffer-crc32/
+bytes/
+camelcase/
+cat-me/
+cliui/
+code-point-at/
+commander/
+connect/
+cookie/
+cookie-signature/
+debug/
+decamelize/
+express/
+formidable/
+fresh/
+invert-kv/
+is-fullwidth-code-point/
+knock-knock-jokes/
+lcid/
+methods/
+mime/
+mkdirp/
+ms/
+nan/
+number-is-nan/
+options/
+os-locale/
+pause/
+policyfile/
+qs/
+range-parser/
+redis/
+send/
+socket.io/
+socket.io-client/
+string-width/
+strip-ansi/
+tinycolor/
+uglify-js/
+window-size/
+wrap-ansi/
+ws/
+xmlhttprequest/
+y18n/
+yargs/
+nasifshah:~/workspace/IntroToNode/DemoApnasifshah:~/workspace/IntroToNode/DemoApp $ node app.js
+                    .............                .""".             .""".
+            ..."""""             """""...       $   . ".         ." .   $
+        ..""        .   .   .   .   .    ..    $   $$$. ". ... ." .$$$   $
+      ."    . " . " . " . " . " . " . " .  "" ."  $$$"""  "   "  """$$$  ".
+    ."      . " . " . " . " . " . " . " .     $  "                    "   $
+   ."   . " . " . "           "   " . " . "  ."      ...          ...     ".
+  ."    . " . "    .."""""""""...     " . "  $     .$"              "$.    $
+ ."     . " . " .""     .   .    ""..   . " $ ".      .""$     .""$      ." $
+."    " . " .       . " . " . " .    $    " $ "      "  $$    "  $$       " $
+$     " . " . " . " . " . " . " . "   $     $             $$.$$             $
+$     " . " . " . " . " . " . " . " .  $  " $  " .        $$$$$        . "  $
+$     " . " . " . " . " . " . " . " .  $    $      "  ..   "$"   ..  "      $
+".    " . " . " . " . " . " . " . "   ."  "  $  . . . $  . .". .  $ . . .  $
+ $    " . " . " . " . " . " . " . "  ."   "            ".."   ".."
+  ".      " . " . " . " . " .  .""    " . " .    """$...         ...$"""
+   ". "..     " . " . " . " .  "........  "    .....  ."""....."""
+     ". ."$".....                       $..."$"$"."   $".$"... `":....
+       "".."    $"$"$"$"""$........$"$"$"  ."."."  ...""      ."".    `"".
+           """.$.$." ."  ."."."    ."."." $.$.$"""".......  ". ". $ ". ". $
+                  """.$.$.$.$.....$.$.""""               ""..$..$."..$..$.
+Knock, knock.
+Who’s there?
+Bed.
+Bed who?
+nasifshah:~/workspace/IntroToNode/DemoApp $ **/
